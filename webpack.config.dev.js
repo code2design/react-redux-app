5e56bfe5f686f777b3d2e67f2f7cff2a297384db
Suprit <suprit@Suprits-MBP.home>
2018-03-10 11:05:00 -0500

    first commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+import webpack                      from 'webpack';
+import path                         from 'path';
+
+
+export default{
+    debug: true,
+    devtool: 'cheap-module-eval-source-map',
+    noInfo: false,
+    entry: [
+        'eventsource-polyfill', //necessary for hotreloading with IE
+        'webpack-hot-middleware/client',
+        './src/index' //entry point of the application
+    ],
+    target: 'web',
+    output: {
+        path: __dirname + '/dist', 
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+
+    },
+    plugins:[
+
+    ],
+    module: {
+        loaders: [
+            
+        ]
+    }
+};
